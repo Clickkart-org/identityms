907a2ce67b9ba61dde4a582a7540c38e9d661bed
asen armour <41690652+asenarmour@users.noreply.github.com>
2024-05-09 22:39:43 +0530

    Update ErrorConstants.java

--- src/main/java/com/clickkart/identityms/contants/ErrorConstants.java
+++ src/main/java/com/clickkart/identityms/contants/ErrorConstants.java
@@ -2,5 +2,5 @@ package com.clickkart.identityms.contants;
 
 public class ErrorConstants {
 
-    public static String UserAlreadyExitsWithEmail = "user already exits with given email";
+    public static String USERALREADYEXITSWITHEMAIL = "User already exits with given email.";
 }
